db577a9943194c3eafa99f54cc2132156dbe69aa
JoyRichardson <84511019+JoyRichardson@users.noreply.github.com>
2021-08-01 19:47:54 -0500

    Commit updated Queries.sql at 7.3.1

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -50,3 +50,41 @@ CREATE TABLE titles (
 );
 SELECT * FROM titles;
 DROP TABLE titles CASCADE;
+--Creating a list of employees born between 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+--Creating a list of employees born in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+--Creating a list of employees born in 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+--Creating a list of employees born in 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+--Creating a list of employees born in 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+--Creating a list of employees born between 1952 and 1955 and hired betwen 1985 and 1988
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+--Count the number of employees born between 1952 and 1955 and hired betwen 1985 and 1988
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+--Creating a table of employees born between 1952 and 1955 and hired betwen 1985 and 1988
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+--View table
+SELECT * FROM retirement_info;
